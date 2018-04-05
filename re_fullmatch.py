7dde544a012bcf983297719365f67b279c701d4d
unknown <vrikshikakalpa@gmail.com>
2018-04-05 09:37:36 +0300

    --fixup

---
+++ re_fullmatch.py
@@ -0,0 +1,12 @@
+import re
+text = 'This is some text -- with punctuation'
+pattern = 'is'
+
+print ('Text   :', text)
+print ('Pattern :', pattern)
+
+m = re.search (pattern, text)
+print('Search   :', m)
+
+s = re.fullmatch(pattern, text)
+print ('Full match:', s)
---
+++ re_search_substring.py
@@ -0,0 +1,20 @@
+import re
+
+text = "This is some text -- with punctuation"
+pattern = re.compile (r'\bw*is\w*\b')
+
+print ('Text :', text)
+print ()
+
+pos = 0
+
+while True:
+    match = pattern.search(text, pos)
+    if not match:
+        break
+    s= match.start()
+    e= match.end()
+    print(' {:>2d} : {:>2d}  = "{}"'.format(
+        s, e - 1, text[s:e]))
+
+    pos = e

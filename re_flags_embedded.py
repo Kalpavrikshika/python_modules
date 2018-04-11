f7bfb2b3b372bca4488089cf47bc1ff7a0e92d40
unknown <vrikshikakalpa@gmail.com>
2018-04-11 13:00:53 +0300

    Embedded flags

---
+++ re_flags_embedded.py
@@ -0,0 +1,9 @@
+import re
+
+text = "This is some text -- with punctuation."
+pattern = r'(?i)\bT\w+'
+regex = re.compile(pattern)
+
+print ('Text     :', text)
+print ('Pattern  :', pattern)
+print ('Matches  :', regex.findall(text))

dcb99ce5662fe376c2bb80d0b5eb712202b052a5
unknown <vrikshikakalpa@gmail.com>
2018-04-24 10:10:18 +0300

    itertools - COMBINATIONS WITH REPLACEMENT

---
+++ itertools_combinations_with_replacement.py
@@ -0,0 +1,17 @@
+from itertools import *
+
+'''each input item is paired with itself as well as other 
+members'''
+
+def show(iterable):
+    first = None
+    for i , item in enumerate(iterable, 1):
+        if first != item[0]:
+            if first is not None:
+                print()
+            first = item[0]
+        print(''.join(item), end=' ')
+    print()
+
+print('Unique pairs:\n')
+show(combinations_with_replacement('abcd' , r=2))

cfde9686c99d9ac8c2ffd8efb8b2a702e35d43c5
unknown <vrikshikakalpa@gmail.com>
2018-05-04 12:16:58 +0300

    numpy_broadcast(a+b)

---
+++ numpy_broadcast2.py
@@ -0,0 +1,18 @@
+import numpy as numpy
+
+
+a = numpy.array([[0.0, 0.0, 0.0],[10.0, 10.0, 10.0],[20.0 ,20.0, 20.0],[30.0, 30.0, 30.0]])
+b = numpy.array([1.0,2.0,3.0])
+
+print ('First array:')
+print (a)
+print ('\n')
+
+print ('Second array:')
+print (b)
+print ('\n')
+
+print ('First Array + Second Array')
+print (a + b)
+
+

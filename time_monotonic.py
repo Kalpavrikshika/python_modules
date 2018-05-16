938267bb3291eb4c3e89c852693f4ddd02c87345
unknown <vrikshikakalpa@gmail.com>
2018-05-16 17:25:02 +0300

    time_monotonic

---
+++ time_monotonic.py
@@ -0,0 +1,9 @@
+import time
+
+start = time.monotonic()
+time.sleep(0.1)
+end = time.monotonic()
+
+print('Start : {:>9.2f}'.format(start))
+print('end   : {:>9.2f}'.format(end))
+print('span  : {:>9.2f}'.format(end - start))

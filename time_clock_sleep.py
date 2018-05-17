5a15ba6655442f1fb9c6845d95af76dc7ce58462
unknown <vrikshikakalpa@gmail.com>
2018-05-17 16:55:06 +0300

    time_clock_sleep

---
+++ time_clock_sleep.py
@@ -0,0 +1,13 @@
+import time
+
+template = '{} - {:0.2f} - {:0.2f}'
+print (template.format(
+    time.ctime(), time.time(), time.clock())
+)
+
+for i in range(3, 0, -1):
+    print('Sleeping', i)
+    time.sleep(i)
+    print(template.format(
+        time.ctime(), time.time(),time.clock())
+        )
